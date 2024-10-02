81ec5fa0a6d7c24eb03658efca9a69c0dcdfccc7
Szymon Bęczkowski <szymon.beczkowski@me.com>
2024-10-02 15:10:11 +0200

    add node position calculation

--- cir2asy.jl
+++ cir2asy.jl
@@ -10,3 +10,6 @@ rNodes = r"^\* node (?P<node_id>\d+)+\s+(?P<net>[\w]+):(?P<node_name>[\w]+)"
 
 lines = readlines("test/parallel_conductors.cir")
 nodes = match.(rNodes, lines) |> filter(!isnothing) .|> Node
+
+nNodes = length(nodes)
+nodeXY(id) = -32, 32id - 16*(nNodes-1)
