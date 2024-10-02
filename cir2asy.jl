f8ef93586ca8346e9a7ccda75448acb916bc65a8
Szymon Bęczkowski <szymon.beczkowski@me.com>
2024-10-02 14:47:20 +0200

    reorder lines

--- cir2asy.jl
+++ cir2asy.jl
@@ -1,14 +1,12 @@
 
-rNodes = r"^\* node (?P<node_id>\d+)+\s+(?P<net>[\w]+):(?P<node_name>[\w]+)"
-
-lines = readlines("test/parallel_conductors.cir")
-
 struct Node
     node_id::Int
     net::String
     node_name::String
 end
-
 Node(m::RegexMatch) = Node(parse(Int, m[:node_id]), m[:net], m[:node_name])
 
+rNodes = r"^\* node (?P<node_id>\d+)+\s+(?P<net>[\w]+):(?P<node_name>[\w]+)"
+
+lines = readlines("test/parallel_conductors.cir")
 nodes = match.(rNodes, lines) |> filter(!isnothing) .|> Node
