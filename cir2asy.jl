209da0235f71bfdf4ec1c5e80d3a86a913d0a4cc
Szymon Bęczkowski <szymon.beczkowski@me.com>
2024-10-03 08:43:27 +0200

    factor out file name

--- cir2asy.jl
+++ cir2asy.jl
@@ -8,7 +8,8 @@ Node(m::RegexMatch) = Node(parse(Int, m[:node_id]), m[:net], m[:node_name])
 
 rNodes = r"^\* node (?P<node_id>\d+)+\s+(?P<net>[\w]+):(?P<node_name>[\w]+)"
 
-lines = readlines("test/parallel_conductors.cir")
+fName = "test/parallel_conductors.cir"
+lines = readlines(fName)
 nodes = match.(rNodes, lines) |> filter(!isnothing) .|> Node
 
 nNodes = length(nodes)
